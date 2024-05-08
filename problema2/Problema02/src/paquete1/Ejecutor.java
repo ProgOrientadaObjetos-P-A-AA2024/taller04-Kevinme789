76feaec85f18f71145e1fa7df4c2ae85e0c7dede
kevin <porkevin743@gmail.com>
2024-05-08 16:42:45 -0500

    Cambios

--- problema2/Problema02/src/paquete1/Ejecutor.java
+++ problema2/Problema02/src/paquete1/Ejecutor.java
@@ -4,17 +4,26 @@
  */
 package paquete1;
 
+import paquete2.Profesores;
+
 /**
  *
  * @author utpl
  */
-public class Problema02 {
+public class Ejecutor {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Profesores profesor1 = new Profesores("Franco", "Aguilar", 1105749223,
+                950.50);
+        profesor1.establecerSueldoTotal();
+        Profesores profesor2 = new Profesores(1102317683, 1500);
+        profesor2.establecerSueldoTotal();
+        System.out.printf("%s", profesor1);
+        System.out.printf("%s", profesor2);
     }
-    
+
 }
--- problema2/Problema02/src/paquete2/Profesores.java
+++ problema2/Problema02/src/paquete2/Profesores.java
@@ -9,8 +9,73 @@ package paquete2;
  * @author utpl
  */
 public class Profesores {
+
     private String nombre;
     private String apellido;
     private double sueldoBasico;
     private double sueldoTotal;
+    private int cedula;
+
+    public Profesores(String n, String a, int c, double s) {
+        nombre = n;
+        apellido = a;
+        cedula = c;
+        sueldoBasico = s;
+    }
+
+    public Profesores(int c, double s) {
+        nombre = "Julio";
+        apellido = "Ordoñez";
+        cedula = c;
+        sueldoBasico = s;
+    }
+
+    public void establecerSueldoBasico(double x) {
+        sueldoBasico = x;
+    }
+
+    public void establecerNombre(String x) {
+        nombre = x;
+    }
+
+    public void establecerApellido(String x) {
+        apellido = x;
+    }
+
+    public void establecerSueldoTotal() {
+        sueldoTotal = sueldoBasico + (sueldoBasico * 0.2);
+    }
+
+    public void establecerCedula(int x) {
+        cedula = x;
+    }
+
+    public double obtenerSueldoBasico() {
+        return sueldoBasico;
+    }
+
+    public double obtenersueldoTotal() {
+        return sueldoTotal;
+    }
+
+    public int obtenerCedula() {
+        return cedula;
+    }
+
+    public String obtenerNombre() {
+        return nombre;
+    }
+
+    public String obtenerApellido() {
+        return apellido;
+    }
+
+    @Override
+    public String toString() {
+        String cadena = String.format("Nombre: %s\nApellido: %s\n"
+                + "Cedula: %d\n"
+                + "Sueldo Basico: %.2f\nSueldo Total: %.2f\n", nombre, apellido,
+                cedula, sueldoBasico, sueldoTotal);
+        return cadena;
+    }
 }
